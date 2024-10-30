5a6e4413dcde9277ae586bcd7385b3f4c5a4d906
Lucas Boucher <contact@lucasboucher.fr>
2024-10-30 11:51:58 +0100

    UPDATE: accept float values on Camera class

--- src/objects/Camera.hpp
+++ src/objects/Camera.hpp
@@ -1,15 +1,14 @@
 #pragma once
+
 #include "../raymath/Vector3.hpp"
 #include "../raymath/Ray.hpp"
 
 class Camera {
+public:
+    Camera(const Vector3& origin, float focal_length);
+    Ray generate_ray(float u, float v, int width, int height) const;
+
 private:
     Vector3 origin;
     float focal_length;
-    float aspect_ratio;
-
-public:
-    Camera(const Vector3& origin = Vector3(0, 0, 0), float focal_length = 1.0f);
-    Ray generate_ray(float x, float y, int width, int height) const;
-    Vector3 get_origin() const;
 };
